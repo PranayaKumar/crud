130c2d9a51e79cb44309e626f3a3fca06cc5b7c3
prny <pranayakg@gmail.com>
2024-02-20 00:27:31 +0530

    update with status msg

--- src/main/java/com/crud/crud2/controller/ProductController.java
+++ src/main/java/com/crud/crud2/controller/ProductController.java
@@ -4,9 +4,12 @@ package com.crud.crud2.controller;
 import com.crud.crud2.entity.Product;
 import com.crud.crud2.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 
@@ -26,13 +29,21 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product>  findAllProduct(){
-       return  service.getProduct();
+    public ResponseEntity<List<Product>> findAllProduct(){
+        List<Product> list = service.getProduct();
+         if(list.size() <= 0){
+             return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+         }
+       return  ResponseEntity.of(Optional.of(list));
     }
     @GetMapping("/productById/{id}")
-    public Product findProductByID(@PathVariable int id){
-        return service.getProductById(id);
-    }
+    public ResponseEntity<Product> findProductByID(@PathVariable int id){
+        Product product =service.getProductById(id);
+        if(product ==null){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(product));
+      }
     @GetMapping("/productByName/{name}")
     public Product findProductByName(@PathVariable String name){
         return service.getProductByName(name);
